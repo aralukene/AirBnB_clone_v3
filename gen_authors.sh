571f7bd2ef8d37c83484c287213dd49b5abc2d7f
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-05-29 12:56:24 -0700

    chore

--- gen_authors.sh
+++ gen_authors.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-set -e
+# Exit immediately if a command exits with a non-zero status.
+set -e 
 
-SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOTDIR="$(git -C "$SCRIPTDIR" rev-parse --show-toplevel)"
+# Get the directory of the current script and store it in the variable SCRIPTDIR.
+SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" 
 
-set -x
+# Get the root directory of the git repository containing the current script and store it in the variable ROOTDIR.
+ROOTDIR="$(git -C "$SCRIPTDIR" rev-parse --show-toplevel)" 
 
-cat > "${ROOTDIR}/AUTHORS" <<- EOF
-	$(git -C "$ROOTDIR" log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf)
+# Print each command before executing it.
+set -x 
+
+# Create or overwrite the file AUTHORS in the root directory of the git repository.
+# Get a list of all authors who have contributed to the git repository and write it to the AUTHORS file.
+cat > "${ROOTDIR}/AUTHORS" <<- EOF 
+ $(git -C "$ROOTDIR" log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf) 
 EOF
